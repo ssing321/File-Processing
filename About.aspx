39b7bef701c59c89ebbb0500c6f8af9c1c20413d
ssing321 <81613102+ssing321@users.noreply.github.com>
2021-06-16 07:46:10 +0530

    Add files via upload

---
+++ About.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="ProjExcel.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="XX-Large" Text="Excel to GridView "></asp:Label>
+    <br />
+    <br />
+    <asp:Label ID="Label2" runat="server" Font-Size="Large" ForeColor="#9966FF" Text="Upload the Excel File and read data using gridview"></asp:Label>
+    <br />
+    <br />
+    <table border="1" class="nav-justified" style="height: 134px; width: 35%; background-color: #00CCFF">
+        <tr>
+            <td style="height: 68px; width: 282px; background-color: #FF99FF">Excel File here </td>
+            <td style="height: 68px">
+                <asp:FileUpload ID="FileUpload1" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td class="modal-sm" style="width: 282px; background-color: #FF99FF">
+                <br />
+            </td>
+            <td>
+                <asp:Button ID="Button1" runat="server" style="background-color: #FFFFFF" Text="Upload" OnClick="Button1_Click" />
+                <br style="background-color: #FF99FF" />
+                <asp:Label ID="Label3" runat="server" style="background-color: #FFFFFF"></asp:Label>
+                <br />
+                <br />
+                <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="UpldDatabase" />
+                <br />
+                <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+    </table>
+    <br />
+
+
+    <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" GridLines="Horizontal" Height="90px" Width="576px">
+        <FooterStyle BackColor="White" ForeColor="#333333" />
+        <HeaderStyle BackColor="#336666" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#336666" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="White" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#339966" Font-Bold="True" ForeColor="White" />
+        <SortedAscendingCellStyle BackColor="#F7F7F7" />
+        <SortedAscendingHeaderStyle BackColor="#487575" />
+        <SortedDescendingCellStyle BackColor="#E5E5E5" />
+        <SortedDescendingHeaderStyle BackColor="#275353" />
+    </asp:GridView>
+
+
+</asp:Content>
---
+++ About.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data.Common;
+
+namespace ProjExcel
+{
+    public partial class About : Page
+    {
+        public DataSet ds_glb = new DataSet(); 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetFileName(FileUpload1.FileName);
+            path = path.Replace(" ", "");
+            FileUpload1.SaveAs(Server.MapPath("~/Upld_files/") + path);
+            String ExcelPath = Server.MapPath("~/Upld_files/") + path;
+            OleDbConnection mycon = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + ExcelPath + "; Extended Properties=Excel 8.0; Persist Security Info = False");
+            mycon.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from [Sheet1$]", mycon);
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ds_glb = ds;
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+            mycon.Close();
+            Label3.Text = "Excel File Has Been Saved and Data Captured";
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=LAPTOP-4FBNESCC\\SQLEXPRESS;Initial Catalog=ExcelDatabase;Integrated Security=True";
+            cn.Open();
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                int val1 = Convert.ToInt32(ds.Tables[0].Rows[i]["Rno"].ToString());
+                String val2 = ds.Tables[0].Rows[i]["FirstName"].ToString();
+                String val3 = ds.Tables[0].Rows[i]["LastName"].ToString();
+                int val4 = Convert.ToInt32(ds.Tables[0].Rows[i]["Marks"].ToString());
+
+                SqlCommand cmd2 = new SqlCommand("insert into StudentDetail(Rno,FirstName,LastName,Marks) values (" + val1 + ",'" + val2 + "','" + val3 + "'," + val4 + ")", cn);
+                cmd2.ExecuteNonQuery();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=LAPTOP-4FBNESCC\\SQLEXPRESS;Initial Catalog=ExcelDatabase;Integrated Security=True";
+            cn.Open();
+
+            for (int i = 0; i < ds_glb.Tables[0].Rows.Count; i++)
+            {
+                int val1 = Convert.ToInt32(ds_glb.Tables[0].Rows[i]["Rno"].ToString());
+                String val2 = ds_glb.Tables[0].Rows[i]["FirstName"].ToString();
+                String val3 = ds_glb.Tables[0].Rows[i]["LastName"].ToString();
+                int val4 = Convert.ToInt32(ds_glb.Tables[0].Rows[i]["Marks"].ToString());
+
+                SqlCommand cmd = new SqlCommand("insert into StudentDetail(Rno,FirstName,LastName,Marks) values (" + val1 + ",'" + val2 + "','" + val3 + "'+" + val3 + ")", cn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
